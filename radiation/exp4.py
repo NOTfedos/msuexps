fa7fbe02c5f11140f1f2a72679f4e1c85356bf0b
NOTfedos <QWErtyuiopl123>
2022-05-12 22:26:35 +0300

    added prak 408

--- radiation/exp4.py
+++ radiation/exp4.py
@@ -1,3 +1,22 @@
-I = (75 * 6.022 * 10 ** 23) / (10**17 * 365 * 24 * 3600 * 18 * 10**(-3))
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 
-print(I)
+fig, ax = plt.subplots()
+xdata, ydata = [], []
+ln, = plt.plot([], [], 'ro')
+
+def init():
+    ax.set_xlim(0, 2*np.pi)
+    ax.set_ylim(-1, 1)
+    return ln,
+
+def update(frame):
+    xdata.append(frame)
+    ydata.append(np.sin(frame))
+    ln.set_data(xdata, ydata)
+    return ln,
+
+ani = FuncAnimation(fig, update, frames=np.linspace(0, 2*np.pi, 128),
+                    init_func=init, blit=True)
+plt.show()
